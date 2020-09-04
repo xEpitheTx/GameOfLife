921308771ef9588599d7a40970c825af6dc3e7e5
Spencer K <thelostdreamer1@gmail.com>
2020-09-04 10:17:10 -0500

    Fin

---
+++ GameOfLife/GifCreator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using ImageMagick;
+using System.Drawing;
+
+namespace GameOfLife
+{
+    public class GifCreator
+    {
+
+        public void CreateGif(int[,] boardState)
+        {
+            using (MagickImageCollection collection = new MagickImageCollection())
+            {
+                // Add first image and set the animation delay (in 1/100th of a second) 
+                CreatePNG();
+                collection.Add("GameOfLife.png");
+                collection[0].AnimationDelay = 100; // in this example delay is 1000ms/1sec
+
+                // Add second image, set the animation delay (in 1/100th of a second) and flip the image
+                CreatePNG();
+                collection.Add("GameOfLife.png");
+                collection[1].AnimationDelay = 100; // in this example delay is 1000ms/1sec
+                //collection[1].Flip();
+
+                // Optionally reduce colors
+                var settings = new QuantizeSettings();
+                settings.Colors = 256;
+                collection.Quantize(settings);
+
+                // Optionally optimize the images (images should have the same size).
+                collection.Optimize();
+
+                // Save gif
+                collection.Write("GameOfLife.Animated.gif");
+            }
+        }
+
+        public void CreatePNG()
+        {
+            // Start the process...  
+            Bitmap memoryImage;
+            memoryImage = new Bitmap(1000, 900);
+            Size s = new Size(memoryImage.Width, memoryImage.Height);
+
+            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+
+            memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
+
+            //That's it! Save the image in the directory and this will work like charm.  
+            string fileName = "GameOfLife.png";
+
+            // save it  
+            memoryImage.Save(fileName);
+        }
+    }
+
+}
--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -9,47 +9,18 @@ namespace GameOfLife
         static void Main(string[] args)
         {
             Console.BackgroundColor = ConsoleColor.DarkBlue;
-            /*
             Console.WriteLine("Enter height");
             int height = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter width");
             int width = Convert.ToInt32(Console.ReadLine());
-            */
             Board board = new Board(25, 25);
             while (true)
             {
                 board.PrintBoardState();
-                Thread.Sleep(1000);
+                Thread.Sleep(750);
                 board.NextBoardState();
                 Console.Clear();
             }
-
-            
-            /*
-            //testing pattern
-            board.PrintBoardState();
-            Thread.Sleep(300);
-            board.NextBoardState();
-            //Console.Clear();
-            board.PrintBoardState();
-            Thread.Sleep(300);
-            board.NextBoardState();
-            //Console.Clear();
-            board.PrintBoardState();
-            Thread.Sleep(300);
-            board.NextBoardState();
-            //Console.Clear();
-            board.PrintBoardState();
-            Thread.Sleep(300);
-            board.NextBoardState();
-            //Console.Clear();
-            board.PrintBoardState();
-            Thread.Sleep(300);
-            board.NextBoardState();
-            //Console.Clear();
-            board.PrintBoardState();
-            */
-            
         }
     }
 }
